b14eea62f8fb33c665d09237ecab9447a361bf2b
Beatriz Heydman <108696912+Beatriz-Heydman@users.noreply.github.com>
2026-07-24 16:50:24 -0300

    feat: create component flex

---
+++ src/components/flex/index.ts
@@ -0,0 +1,14 @@
+// Libs
+import styled from "styled-components";
+
+// Types
+import type { FlexProps } from "./types";
+
+export const Flex = styled.div<FlexProps>`
+  display: flex;
+  gap: ${({ gap = "0" }) => gap};
+  justify-content: ${({ justifyContent = "center" }) => justifyContent};
+  align-items: ${({ alignItems = "center" }) => alignItems};
+  flex-wrap: ${({ flexWrap = "nowrap" }) => flexWrap};
+  flex-direction: ${({ direction = "row" }) => direction};
+`;
---
+++ src/components/flex/types.ts
@@ -0,0 +1,7 @@
+export type FlexProps = {
+    gap?: string;
+    justifyContent?: "flex-start" | "flex-end" | "center" | "space-between" | "space-around";
+    alignItems?: "flex-start" | "flex-end" | "center" 
+    flexWrap?: "nowrap" | "wrap" | "wrap-reverse";
+    direction?: "row" | "column"
+}
