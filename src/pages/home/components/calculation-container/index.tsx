4863323a88aef04ef5bd8e57418fb5e3bbe9b356
Beatriz Heydman <108696912+Beatriz-Heydman@users.noreply.github.com>
2026-07-25 00:20:18 -0300

    feat: create component calculation-container

---
+++ src/pages/home/components/calculation-container/index.tsx
@@ -0,0 +1,11 @@
+// Styles
+import { Input } from "../../../../components/input";
+import { StyledCalculationContainer } from "./styles";
+
+export function CalculationContainer() {
+  return (
+    <StyledCalculationContainer>
+      <Input />
+    </StyledCalculationContainer>
+  );
+}
---
+++ src/pages/home/components/calculation-container/styles.ts
@@ -0,0 +1,13 @@
+// Libs
+import styled from "styled-components";
+
+export const StyledCalculationContainer = styled.div`
+  width: 70%;
+  min-height: 300px;
+  background-color: #fefefe;
+  border-radius: 0.75rem;
+  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.22);
+  display: flex;
+  position: absolute;
+  top: 50%;
+`;
