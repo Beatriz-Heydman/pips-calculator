a139b43b75060f3ae5431922f16ffcd1fb6dc7ad
Beatriz Heydman <108696912+Beatriz-Heydman@users.noreply.github.com>
2026-07-24 23:34:19 -0300

    fix: adjust style

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -10,19 +10,22 @@ import { StyledHeader } from "./styles";
 
 export function Header() {
     return <StyledHeader>
-        <Flex direction= "column" gap= "1.5rem" justifyContent= "flex-start" alignItems= "flex-start" style={{padding: "2.5rem"}}>
-           <div className= "logo">
-             <LuChartNoAxesCombined size={38} color="#fff"/>
-           </div>
+        <Flex direction= "column" gap= "1.5rem" justifyContent= "flex-start" alignItems= "center" style={{padding: "2.5rem", width: "55%"}}>
            
-           <Flex direction= "column" gap= "0.5rem" justifyContent= "center" alignItems= "flex-start">
+           <Flex justifyContent= "flex-start" alignItems= "center" style={{width: "100%"}}>
+              <div className= "logo">
+                 <LuChartNoAxesCombined size={38} color="#fff"/>
+              </div>
+           </Flex>
+           
+           <Flex direction= "column" gap= "0.5rem" justifyContent= "center" alignItems= "flex-start" style={{width: "100%"}}>
             <Typography fontSize="3rem" fontWeight="600">Calculadora de Lote Forex</Typography>
             <Typography fontSize= "1.15rem" fontWeight="300" lineHeight="1.5">Descubra o tamanho ideal do lote com base na 
                 <br/> quantidade de pips e no valor máximo da operação.</Typography>
            </Flex>
         </Flex>
 
-        <Flex>
+        <Flex  alignItems= "flex-start">
             <img src="public/assets/images/candlestick.png" alt="Header Image" className= "header-image" />
         </Flex>
     </StyledHeader>
--- src/components/header/styles.ts
+++ src/components/header/styles.ts
@@ -2,28 +2,28 @@
 import styled from "styled-components";
 
 export const StyledHeader = styled.header`
- width: 100%;
- height: 400px;
- display: flex;
- justify-content: space-between;
-//  background-color: #000920;
- background-image: linear-gradient(to right, #000920, #16196d);
+  width: 100%;
+  height: 400px;
+  display: flex;
+  justify-content: flex-end;
+  align-items: flex-start;
+  position: relative;
+  background-image: linear-gradient(to right, #000920, #16196d);
 
- .logo {
- width: 60px;
- height: 60px;
- display: flex;
- justify-content: center;
- align-items: center;
- background-color: #2f39df;
- border-radius: 0.5rem;
- }
+  .logo {
+    width: 60px;
+    height: 60px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #2f39df;
+    border-radius: 0.5rem;
+  }
 
- .header-image {
- width: 100%;
- height: 400px;
- object-fit: cover;
- opacity: 0.3;
- }
-
-`
+  .header-image {
+    width: 500px;
+    height: 300px;
+    object-fit: cover;
+    opacity: 0.3;
+  }
+`;
