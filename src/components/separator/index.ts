8465fc163e4144a0ea62998b40af4215c9f83565
Beatriz Heydman <108696912+Beatriz-Heydman@users.noreply.github.com>
2026-07-30 00:08:32 -0300

    feat: create component separator

---
+++ src/components/separator/index.ts
@@ -0,0 +1,11 @@
+// Libs
+import styled from "styled-components";
+
+// Types
+import type { SeparatorProps } from "./types";
+
+export const Separator = styled.div<SeparatorProps>`
+  min-width: 1px !important;
+  height: 80px;
+  background: ${({ color = "#d5dbea" }) => color};
+`;
