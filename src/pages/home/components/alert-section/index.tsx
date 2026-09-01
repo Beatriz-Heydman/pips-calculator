692e3ba88bee5aa479afcb51146144845c4626c8
Beatriz Heydman <108696912+Beatriz-Heydman@users.noreply.github.com>
2026-09-01 19:47:51 -0300

    feat: create component alert-section

---
+++ src/pages/home/components/alert-section/index.tsx
@@ -0,0 +1,28 @@
+// Libs
+import { IoInformationCircle } from 'react-icons/io5';
+import { PiCalculatorLight } from 'react-icons/pi';
+
+// Components
+import { Typography } from '@components';
+
+// Styles
+import { StyledAlertSection } from './styles';
+
+export function AlertSection() {
+  return (
+    <StyledAlertSection>
+      <div className="icon-container">
+        <IoInformationCircle size={20} color="#fff" />
+      </div>
+
+      <Typography color="#3c4257" fontSize="0.9rem" fontWeight="400">
+        Os cálculos são aproximados e podem variar de acordo com o par de
+        moedas, o valor do pip e a corretora utilizada.
+      </Typography>
+
+      <div className="decorative-icon">
+        <PiCalculatorLight size={24} color="#a9b7e6" />
+      </div>
+    </StyledAlertSection>
+  );
+}
---
+++ src/pages/home/components/alert-section/styles.ts
@@ -0,0 +1,32 @@
+// Libs
+import styled from 'styled-components';
+
+export const StyledAlertSection = styled.div`
+  width: 100%;
+  max-width: 92vw;
+  margin: 1.5rem 0 3rem;
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  background-color: #e3efff;
+  border-radius: 0.75rem;
+  padding: 1.25rem 1.5rem;
+
+  .icon-container {
+    width: 32px;
+    height: 32px;
+    min-width: 32px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50%;
+    background-color: #2530d8;
+  }
+
+  .decorative-icon {
+    margin-left: auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
