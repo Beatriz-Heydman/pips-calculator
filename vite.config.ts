d14f552a7f7220a272b5ba1658e8696ce14013ae
Beatriz Heydman <108696912+Beatriz-Heydman@users.noreply.github.com>
2026-09-02 23:59:17 -0300

    feat: add PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
@@ -7,7 +8,37 @@ const dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['assets/icons/calculator-crypto.png'],
+      manifest: {
+        name: 'Calculadora de Pips',
+        short_name: 'Pips Calculator',
+        description:
+          'Calculadora do valor de operações de trade a partir do valor do pip, quantidade de pips e valor máximo de operação.',
+        lang: 'pt-BR',
+        theme_color: '#2530d8',
+        background_color: '#fefefe',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/assets/icons/calculator-crypto.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: '/assets/icons/calculator-crypto.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@components': path.resolve(dirname, './src/components'),
