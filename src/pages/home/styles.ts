b6aabf51953a77a653f601dde864c155f901e919
Beatriz Heydman <108696912+Beatriz-Heydman@users.noreply.github.com>
2026-09-03 00:11:06 -0300

    refactor: adjust home view padding bottom

--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -8,4 +8,12 @@ export const StyledHomeView = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+
+  @media (min-width: 769px) {
+    padding-bottom: 2rem;
+  }
+
+  @media (max-width: 768px) {
+    padding-bottom: 1.5rem;
+  }
 `;
